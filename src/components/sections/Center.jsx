a535757764c87c90b78296e195c22fca951a241d
Brian07052024 <briancorpusramirez2@hotmail.com>
2025-12-28 22:56:57 -0600

    fix: Update tip message in Center component for better guidance on animated mode settings

--- src/components/sections/Center.jsx
+++ src/components/sections/Center.jsx
@@ -59,7 +59,7 @@ function Center() {
                         <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
                     </svg>
                     <p className="text-xs lg:text-sm">
-                        <span className="font-semibold">Tip:</span> Para mejores resultados en modo animado, elige un color inicial claro en tu paleta.
+                        <span className="font-semibold">Tip:</span> Para mejores resultados en modo animado, no sobrepases 15% de granulado, y elige un color inicial claro en tu paleta.
                     </p>
                     <button
                         onClick={() => setShowTip(false)}
